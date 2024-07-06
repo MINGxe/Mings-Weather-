40f73007b9bdd990c5e5285fb2462bf67d552188
XIE MING <xieming@XIEs-MacBook-Pro.local>
2024-07-06 16:20:33 +1000

    add date function

--- check-weather.py
+++ check-weather.py
@@ -1,6 +1,6 @@
 import openmeteo_requests
 
-from datetime import date
+from datetime import date, datetime
 
 import requests
 import urllib
@@ -13,16 +13,26 @@ def get_the_weather_data():
 
     location_name = sys.argv[1]
 
-    print (location_name)
-
     url = "http://api.open-meteo.com/v1/forecast"
 
     location = print_my_location(location_name)
 
+    input_date_text = sys.argv[2]
+    target_date = datetime.strptime(input_date_text, '%d/%m/%Y')
+
+
+    MAX_DAYS_FROM_TODAY = 16
+    days_from_today = (target_date.date() - date.today()).days
+
+    if ((days_from_today > MAX_DAYS_FROM_TODAY) or (days_from_today < 0)):
+       print(f'Data not available for {target_date.date()}')
+       exit(code=1)
+
     params = {
         "latitude":location['latitude'],
         "longitude":location['longitude'],
-        "daily": ["temperature_2m_max", "temperature_2m_min"]
+        "daily": ["temperature_2m_max", "temperature_2m_min"],
+        "forecast_days": MAX_DAYS_FROM_TODAY
     }
 
     responses = openmeteo.weather_api(url, params=params)
@@ -32,9 +42,9 @@ def get_the_weather_data():
     daily = response.Daily()
     daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
     daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
-    return {"tempretures_high":daily_temperature_2m_max[0],
-    "tempretures_low":daily_temperature_2m_min[0],
-    "Date":date.today(),
+    return {"tempretures_high":daily_temperature_2m_max[days_from_today],
+    "tempretures_low":daily_temperature_2m_min[days_from_today],
+    "Date":target_date.date(),
     "Location":location_name}
 
 def print_the_weather(weather_data):
@@ -44,8 +54,10 @@ def print_the_weather(weather_data):
     print(f"Tempretures_low: {weather_data['tempretures_low']} Celsus")
 
 
-
-
+def print_my_date():
+    # Input examples:
+    # 20/07/2024
+    print('')
 
 
 def print_my_location(location_name):
@@ -56,12 +68,11 @@ def print_my_location(location_name):
     }
     response = requests.get(f'{url}?{urllib.parse.urlencode(params)}')
     json_data = response.json()
-    print (json_data)
-    
+
     return {'latitude': json_data['results'][0]['latitude'],
     'longitude': json_data['results'][0]['longitude']}
 
-    
+
 
 
 
@@ -73,6 +84,7 @@ def print_my_location(location_name):
 
 weather_data=get_the_weather_data()
 print_the_weather(weather_data)
+
 
 
    
